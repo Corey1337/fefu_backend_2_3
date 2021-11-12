69043f0c8f99cf5cdf28ce2cfd6723d2f622e346
Corey1337 <31986680+Corey1337@users.noreply.github.com>
2021-11-12 21:19:23 +1000

    Lesson 6: Validate and sanitize appeal form input (#8)
    * Created new migrations for appeal form
    
    * added sanitizer for phone, validation
    
    * migrations fix
    
    * migration fix, num_sanitizer fix

--- app/Http/Controllers/AppealController.php
+++ app/Http/Controllers/AppealController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AppealPostRequest;
 use App\Models\Appeal;
 use Illuminate\Http\Request;
+use App\Sanitizers\PhoneSanitizer;
 
 class AppealController extends Controller
 {
@@ -15,51 +17,26 @@ class AppealController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $errors = [];
-        $success = $request->session()->get('success', false);
 
         if($request->isMethod('POST')) {
-            $name = $request->input('name');
-            $phone = $request->input('phone');
-            $email = $request->input('email');
-            $message = $request->input('message');
 
-            if ($name === null) {
-                $errors['name'] = 'Fill the "name" field';
-            }
+            $validate = $request->validate(AppealPostRequest::rules());
 
-            if($phone === null && $email === null) {
-                $errors['contacts'] = 'Fill in at least one of the fields';
-            }
+            $appeal = new Appeal();
+            $appeal->name = $validate['name'];
+            $appeal->surname = $validate['surname'];
+            $appeal->patronymic = $validate['patronymic'];
+            $appeal->age = $validate['age'];
+            $appeal->gender = $validate['gender'];
+            $appeal->phone = PhoneSanitizer::num_sanitize($validate['phone']);
+            $appeal->email = $validate['email'];
+            $appeal->message = $validate['message'];
+            $appeal->save();
 
-            if(strlen($phone) !== 11) {
-                $errors['phone'] = 'Phone lenth must be 11 characters';
-            }
-
-            if ($message === null) {
-                $errors['message'] = 'Fill the "message" field';
-            }
-
-            if (count($errors) > 0) {
-                $request->flash();
-            } else {
-                $appeal = new Appeal();
-                $appeal->name = $name;
-                $appeal->phone = $phone;
-                $appeal->email = $email;
-                $appeal->message = $message;
-                $appeal->save();
-
-                $success = true;
-
-                return redirect()
-                    ->route('appeal')
-                    ->with('success', $success);
-            }
-
-            
+            return redirect()
+                ->route('appeal');            
         }
 
-        return view('appeal', ['errors' => $errors, 'success' => $success]);
+        return view('appeal');
     }
 }
